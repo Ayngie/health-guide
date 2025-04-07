de26483757ba25d7beeb9abaa7dcaa7a5b095a3c
Ayngie <angelicareutersward@gmail.com>
2025-04-07 21:54:55 +0200

    Refactor: removed unnused duplicate styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 const Home: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <article className="text-center bg-teal-50">
+    <article className="text-center">
       <h2 className="text-5xl mb-6">{t("home.welcome")}!</h2>
       <h3 className="text-xl">{t("home.welcomeText")}</h3>
     </article>
