e0c912054dc3f643e7c4e4d3235ecbae9d9a36a0
Ayngie <angelicareutersward@gmail.com>
2025-04-07 22:16:07 +0200

    Feat: broke out components + made "logo" link to home

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from "react-i18next";
+import SearchForm from "./SearchForm";
+import DarkModeToggle from "./DarkModeToggle";
+import Nav from "./Nav";
+import LanguageToggle from "./LanguageToggle";
+
+const Header = () => {
+  const { t } = useTranslation();
+
+  return (
+    <header>
+      <section className="p-4 flex flex-col sm:flex-row justify-between items-center border-b dark:border-gray-700 gap-4">
+        {/* Page "Logo" */}
+        <a href="/">
+          <h1 className="text-4xl p-4 font-bold underline">{t("appTitle")}</h1>
+        </a>
+
+        {/* Search item on site */}
+        <SearchForm />
+
+        {/* Toggles */}
+        <div className="flex gap-4">
+          <DarkModeToggle />
+          <LanguageToggle />
+        </div>
+      </section>
+
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
