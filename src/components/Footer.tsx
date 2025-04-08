6e899560a1bdfd9ce6a0ddf1b3b54b8233d6921f
Ayngie <angelicareutersward@gmail.com>
2025-04-08 17:02:41 +0200

    Refactor: Removed fixed footer, now that I am using flexbox with flex-grow on main

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentDate = new Date().getFullYear();
   return (
-    <footer className="p-5 w-screen fixed bottom-0 left-0 text-center border-t dark:border-gray-700">
+    <footer className="p-5 text-center border-t dark:border-gray-700">
       &copy; Copyright Ayngie {currentDate}
     </footer>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,10 +10,10 @@ if (savedLanguage) {
 
 const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
+    <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
       <Header />
 
-      <main className="p-10 bg-teal-50 dark:bg-gray-800">
+      <main className="flex-grow p-10 bg-teal-50 dark:bg-gray-800">
         <Outlet />
       </main>
 
