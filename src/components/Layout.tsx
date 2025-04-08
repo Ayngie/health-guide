c69f6ea307cff87c6eb084866e355e2741046801
Ayngie <angelicareutersward@gmail.com>
2025-04-08 17:03:02 +0200

    Style: added responsivity to padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ const Layout: React.FC = () => {
     <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
       <Header />
 
-      <main className="flex-grow p-10 bg-teal-50 dark:bg-gray-800">
+      <main className="flex-grow p-4 sm:p-10 bg-teal-50 dark:bg-gray-800">
         <Outlet />
       </main>
 
