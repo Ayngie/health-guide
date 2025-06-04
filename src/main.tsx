7a5797c0f02ffd49cd377b130ad172257a8f7748
Angie <71646930+Ayngie@users.noreply.github.com>
2025-06-04 13:31:24 +0200

    Update main.tsx to include basename in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./lib/i18n"; //Import to insure i18n initializes
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/health-guide/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
